0ee25b21cfcad060e896cab353b0cb69cc253cf5
Mutegeki <braynmute@gmail.com>
2018-11-17 18:42:39 +0300

    Added a signup page

--- mysite/articles/urls.py
+++ mysite/articles/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'articles'
+
 urlpatterns = [
-    url(r'^$', views.article_list),
+    url(r'^$', views.article_list, name='list'),
 ]
