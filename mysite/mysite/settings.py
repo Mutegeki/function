4f144050b69a9eaf207a2f70179db780f84f8b1f
Mutegeki <braynmute@gmail.com>
2018-11-17 06:14:51 +0300

    Made migrations

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -74,19 +74,20 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'USER': 'function',
- #       'PASSWORD': 'function.123',
-##    }
+ #       'ENGINE': 'django.db.backends.sqlite3',
+ #       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+ #   }
 #}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'function_db',
+        'USER': 'articles',
+        'PASSWORD': 'Articles.123',
+    }
+}
 
 
 
